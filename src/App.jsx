9509ec522c5aad87fd6f4af6ceeaa6a542d1f437
thesandeepkonda <kondasandeep56@gmail.com>
2026-05-25 18:40:39 +0530

    update hashbrower

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 import Revamp from './pages/Revamp';
 import Native from './pages/Native';
