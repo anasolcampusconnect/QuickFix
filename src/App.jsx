5ff20facacefebfabc1ae9da699b6a6a83b0d5c4
thesandeepkonda <kondasandeep56@gmail.com>
2026-05-25 18:44:49 +0530

    update hashmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Revamp from './pages/Revamp';
 import Native from './pages/Native';
